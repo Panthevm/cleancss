a2ff98d8a2b46f44ecd730f0029e40bf8aec39e5
panthevm <panthevm@yandex.ru>
2021-01-24 12:17:51 +0300

    Update example

--- example/src/app/core.cljs
+++ example/src/app/core.cljs
@@ -1,17 +1,19 @@
  (ns ^:figwheel-hooks app.core
-  (:require        [reagent.dom :as dom])
-  (:require-macros [cleancss.utils :refer (c)]))
+   (:require
+    [reagent.dom    :as dom]
+    [cleancss.utils :refer [c]]))
+
 
 (defn view
   []
-  [:section {:class (c ["min-h-screen" "flex" "items-center" "justify-around" "bg-red-500"])}
-   [:div {:class (c ["h-32" "w-32" "relative" "cursor-pointer"])}
-    [:div {:class (c ["absolute" "inset-0" "bg-white" "opacity-25" "rounded-lg" "shadow-2xl"])}]
-    [:div {:class (c ["absolute" "inset-0" "transform" "hover:scale-75" "transition"])}
-     [:div {:class (c ["h-full" "w-full" "bg-white" "rounded-lg" "shadow-2xl"])}]]]])
+  [:section {:class (c "min-h-screen" "flex" "items-center" "justify-around" "bg-red-500")}
+   [:div {:class (c "h-32" "w-32" "relative" "cursor-pointer")}
+    [:div {:class (c "absolute" "inset-0" "bg-white" "opacity-25" "rounded-lg" "shadow-2xl")}]
+    [:div {:class (c "absolute" "inset-0" "transform" "hover:scale-75" "transition")}
+     [:div {:class (c "h-full" "w-full" "bg-white" "rounded-lg" "shadow-2xl")}]]]])
 
 (defn ^:after-load mount
-  [& args]
+  []
   (dom/render [view] (js/document.getElementById "app")))
 
 (mount)
