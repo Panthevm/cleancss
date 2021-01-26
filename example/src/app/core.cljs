c5b91c7301e555cf3d3f21ff5ad2ef555f7edd90
panthevm <panthevm@yandex.ru>
2021-01-26 10:29:55 +0300

    Typo fix

--- example/src/app/core.cljs
+++ example/src/app/core.cljs
@@ -1,14 +1,11 @@
 (ns ^:figwheel-hooks app.core
   (:require
    [reagent.dom    :as dom]
-   [app.test       :as test]
    [cleancss.utils :refer [c]]))
 
 (defn view
   []
-  [:<>
-   [test/view]
-   [:section {:class (c "min-h-screen" "bg-red-500")}]])
+  [:section {:class (c "min-h-screen" "bg-red-500")}])
 
 (defn ^:after-load mount
   []
